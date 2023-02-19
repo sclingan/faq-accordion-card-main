d3214320127ec63b1b9004dd931a7a841b18965d
Scott Clingan <sclingan1@gmail.com>
2023-02-19 00:01:40 -0600

    Adding javascript for details and summary

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const question = document.getElementsByTagName('details');
-console.log(question);
 
 
-question.forEach(e => {
-question[e].addEventListener("toggle", (event) => {
-    if (question[e].open) {
-      /* the element was toggled open */
-      console.log('open');
-    } else {
-      /* the element was toggled closed */
-      console.log('closed');
-    }
-  })
-});
+const questionArray = Array.from(question);
+
+questionArray.forEach(e => e.addEventListener('toggle', (event) =>
+{
+ console.log(e);
+}
+));
