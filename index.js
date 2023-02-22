5f39e7712581614930d871e60ac7e8c9aa559326
Scott Clingan <sclingan1@gmail.com>
2023-02-22 13:53:25 -0600

    Adjusting desktop layout

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
+/* If summary is open and a new details is clicked,
+   close all other summary
+   */
 
 var details = [...document.querySelectorAll('details')];
 document.addEventListener('click', function(e) {
